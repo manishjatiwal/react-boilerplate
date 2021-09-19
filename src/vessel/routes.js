2cb69ccbdb748ef4f75b0d63d2869f99352b0793
Manish Jatiwal <manish.s.jatiwal@gmail.com>
2021-09-19 20:07:05 +0530

    feat: Adding resume app

--- src/vessel/routes.js
+++ src/vessel/routes.js
@@ -1,3 +1,5 @@
+import Resume from 'applications/resume'
+import TaskManager from 'applications/task-manager'
 import TestApplication from 'applications/test'
 
 const routes = [
@@ -5,6 +7,16 @@ const routes = [
     path: '/test',
     component: TestApplication,
     title: 'Test App'
+  },
+  {
+    path: '/resume',
+    component: Resume,
+    title: 'Resume'
+  },
+  {
+    path: 'task-manager',
+    component: TaskManager,
+    title: 'Task Manager'
   }
 ]
 
